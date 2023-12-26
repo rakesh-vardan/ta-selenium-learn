812880132b7ed58565260cddeacb25df362150ea
Rakesh Vardan <rakeshbudugu@gmail.com>
2023-12-26 13:29:50 +0530

    First commit

---
+++ src/test/java/io/learn/FirstTest.java
@@ -0,0 +1,24 @@
+package io.learn;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class FirstTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new FirefoxDriver();
+//        WebDriver driver = new EdgeDriver();
+//        WebDriver driver = new SafariDriver();
+        driver.get("https://www.selenium.dev");
+
+        System.out.println("Current page title is: " + driver.getTitle()); //Selenium
+        System.out.println("Current page URL is: " + driver.getCurrentUrl()); //https://www.selenium.dev
+
+        driver.close();
+    }
+}
